022300c82588d5c6bcdf3ec20ad49413ecb9ce12
Ardac <138930253+ardacy2112@users.noreply.github.com>
2025-04-13 23:48:01 +0300

    Update free-gen.js

--- commands/free-gen.js
+++ commands/free-gen.js
@@ -11,7 +11,7 @@ module.exports = {
       option.setName('option')
         .setDescription('Choose a service.')
         .setRequired(true)
-        .addChoice("STEAM", "STEAM" "BRAWL STARS", "BRAWL STARS" "SPOTİFY", "SPOTİFY") // Add options here
+        .addChoice("STEAM", "STEAM"; "BRAWL STARS", "BRAWL STARS"; "SPOTİFY", "SPOTİFY") // Add options here
     ),
   async execute(interaction) {
     const allowedChannelId = process.env.FGEN_CHANNEL_ID;
